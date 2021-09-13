af12e5c7b3a6a21d928ee66d74e9c324e6a39a6a
TexRexTex <tanebosman@gmail.com>
2021-09-14 00:30:40 +1200

    Add base code for the bot

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.io'),
+  auth = require('./auth.json'),
+  client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(client.user.presence.status);
+})
+
+client.on('message', msg => {
+
+}
+
+client.login(auth.token);
